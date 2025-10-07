27d4fe4de54e613abf93c0408f4e496e8b918599
chadakrishnakrupa <48622326+chadakrishnakrupa@users.noreply.github.com>
2025-10-07 15:16:39 +0530

    Modified to sprint boot application

---
+++ src/main/java/com/kk/helloworldapplication/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.kk.helloworldapplication
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  
+public class HelloWorld {
+  public static void main(String[] args) {
+    SpringApplication.run(HelloWorld.class, args);
+  }
+}
